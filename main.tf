72ef903fec1ebae7497368615ac0e5f22ff92213
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-03-20 17:33:10 +0530

    route53

--- main.tf
+++ main.tf
@@ -22,3 +22,17 @@ resource "aws_instance" "Sonarqube" {
     Name = "Sonarqube_Instance"
   }
 }
+
+
+data "aws_route53_zone" "selected" {
+  name         = "robofarming.link"
+  private_zone = false
+}
+
+resource "aws_route53_record" "domainName" {
+  name    = "sonar"
+  type    = "A"
+  zone_id = data.aws_route53_zone.selected.zone_id
+  records = [aws_instance.Sonarqube.public_ip]
+  ttl     = 300
+}

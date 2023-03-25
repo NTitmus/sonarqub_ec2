d702744ef334dbb2a0dce4e141b480f5f3ce35d8
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-03-25 12:33:43 +0530

    updated the comments

--- sonar_script.sh
+++ sonar_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Increase the vm.max_map_count for kernel and ulimit for the current session at runtime.
 sudo bash -c 'cat <<EOT> /etc/sysctl.conf
 vm.max_map_count=524288
 fs.file-max=131072
@@ -9,24 +9,23 @@ EOT'
 sudo sysctl -w vm.max_map_count=524288
 sysctl -w fs.file-max=131072
 
+#Increase these permanently
 sudo bash -c 'cat <<EOT> /etc/security/limits.conf
 sonarqube   -   nofile   65536
 sonarqube   -   nproc    4096
 EOT'
 
+# Need JDK 17 or higher to run SonarQube 9.9
 sudo apt-get update -y
 sudo apt-get install openjdk-17-jdk -y
-
 sudo update-alternatives --config java
-
 java -version
 
-sudo apt update
-wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 
+# Install and configure PostgreSQL & Create a user and database for sonar
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt install postgresql postgresql-contrib -y
-#sudo -u postgres psql -c "SELECT version();"
 sudo systemctl enable postgresql.service
 sudo systemctl start  postgresql.service
 sudo echo "postgres:admin123" | chpasswd
@@ -36,7 +35,8 @@ sudo -i -u postgres psql -c "CREATE DATABASE sonarqube OWNER sonar;"
 sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE sonarqube to sonar;"
 sudo systemctl restart  postgresql
 
-netstat -tulpena | grep postgres
+
+#Download the binaries for SonarQube 
 sudo mkdir -p /sonarqube/
 cd /sonarqube/
 sudo curl -O https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.0.65466.zip
@@ -45,6 +45,8 @@ sudo unzip -o sonarqube-9.9.0.65466.zip -d /opt/
 sudo mv /opt/sonarqube-9.9.0.65466/ /opt/sonarqube
 sudo rm -rf /opt/sonarqube/conf/sonar.properties
 sudo touch /opt/sonarqube/conf/sonar.properties
+
+# PostgreSQL database username and password
 sudo bash -c 'cat <<EOT> /opt/sonarqube/conf/sonar.properties
 sonar.jdbc.username=sonar
 sonar.jdbc.password=admin123
@@ -56,11 +58,12 @@ sonar.search.javaOpts=-Xmx512m -Xms512m -XX:+HeapDumpOnOutOfMemoryError
 sonar.log.level=INFO
 sonar.path.logs=logs
 EOT'
-
+# Create the group
 sudo groupadd sonar
 sudo useradd -c "SonarQube - User" -d /opt/sonarqube/ -g sonar sonar
 sudo chown sonar:sonar /opt/sonarqube/ -R
 
+# Create a systemd service file for SonarQube to run at system startup
 sudo touch /etc/systemd/system/sonarqube.service
 sudo bash -c 'cat <<EOT> /etc/systemd/system/sonarqube.service
 [Unit]
@@ -85,10 +88,12 @@ LimitNPROC=4096
 WantedBy=multi-user.target
 EOT'
 
+# automatically system startup enable
 sudo systemctl daemon-reload
 sudo systemctl enable sonarqube.service
-#systemctl start sonarqube.service
-#systemctl status -l sonarqube.service
+
+# Install and configure Nginx as a reverse proxy for SonarQube.
+
 apt-get install nginx -y
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo rm -rf /etc/nginx/sites-available/default
@@ -117,6 +122,8 @@ server{
 }
 EOT'
 ln -s /etc/nginx/sites-available/sonarqube /etc/nginx/sites-enabled/sonarqube
+
+# Automatically system startup enable
 sudo systemctl enable nginx.service
 sudo systemctl restart nginx.service
 sudo systemctl restart sonarqube.service

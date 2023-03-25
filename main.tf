6d88a19ee9cef2ea7654e9907bad506055368960
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-03-25 12:37:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "Sonarqube" {
-  ami                    = "ami-0557a15b87f6559cf"
+  ami                    = "ami-0557a15b87f6559cf" # free tier AMI image
   instance_type          = "t2.medium"
   user_data              = file("sonar_script.sh")
   vpc_security_group_ids = [aws_security_group.ec2.id]
-  key_name               = "new-eks"
+  key_name               = "new-eks" # Existing ssh key 
 
   tags = {
     Name = "Sonarqube_Instance"
